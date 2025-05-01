307a4fd026d4c9039951b15f9c6507b2f3a22db0
Levaughn Hines <lhines2000@gmail.com>
2025-05-01 16:29:21 -0500

    render version

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
-from flask_cors import CORS  
+from flask_cors import CORS  # ✅ CORS import
 from .config import Config
 
 # Initialize app and extensions
