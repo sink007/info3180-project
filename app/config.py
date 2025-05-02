5084daa24636bc83afeea0a5c26800f325b1978c
Jaheim Walker <walkerjaheim876@gmail.com>
2025-05-02 12:32:33 -0500

    updated config

--- app/config.py
+++ app/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', os.path.join(os.getcwd(), 'uploads'))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your-jwt-secret')
