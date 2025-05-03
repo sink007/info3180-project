d9d9798451b2ef5017ee5782354f8136afd01307
BrawlyBotMrJ <129111086+BrawlyBotMrJ@users.noreply.github.com>
2025-05-03 13:58:11 -0500

    reverted config.py change

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
 
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'app', 'static', 'uploads')
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your-jwt-secret')
