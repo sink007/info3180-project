9f8f3477796d006de91d15f429e29f845b802963
Jaheim Walker <walkerjaheim876@gmail.com>
2025-05-03 07:45:19 -0500

    bug fixes

--- app/__init__.py
+++ app/__init__.py
@@ -15,13 +15,13 @@ migrate = Migrate(app, db)
 from .models import Users, Profile, Favourite
 from app import views
 
-@app.route('/')
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def serve_vue(path=''):
+def serve_vue(path):
     static_folder = os.path.join(os.path.dirname(__file__), 'static')
-    target = os.path.join(static_folder, path)
+    file_path = os.path.join(static_folder, path)
 
-    if path != "" and os.path.exists(target):
+    if path != "" and os.path.exists(file_path) and not os.path.isdir(file_path):
         return send_from_directory(static_folder, path)
-    else:
-        return send_from_directory(static_folder, 'index.html')
+
+    return send_from_directory(static_folder, 'index.html')
