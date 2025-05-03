3b35c924438013b7ba386fc061e6e4f351c1a458
Jaheim Walker <walkerjaheim876@gmail.com>
2025-05-03 07:08:20 -0500

    fixed refreshing crash

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,15 @@ migrate = Migrate(app, db)
 
 from .models import Users, Profile, Favourite
 from app import views
+
+
+@app.route('/')
+@app.route('/<path:path>')
+def serve_vue(path=''):
+    static_folder = os.path.join(os.path.dirname(__file__), 'static')
+    target = os.path.join(static_folder, path)
+    
+    if path != "" and os.path.exists(target):
+        return send_from_directory(static_folder, path)
+    else:
+        return send_from_directory(static_folder, 'index.html')
