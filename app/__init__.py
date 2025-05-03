098b042713b31f9792c359a4d445500161766405
Jaheim Walker <walkerjaheim876@gmail.com>
2025-05-03 08:01:29 -0500

    currently removed feature

--- app/__init__.py
+++ app/__init__.py
@@ -16,13 +16,3 @@ from .models import Users, Profile, Favourite
 from app import views
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve_vue(path):
-    static_folder = os.path.join(os.path.dirname(__file__), 'static')
-    target = os.path.join(static_folder, path)
-
-    if path != "" and os.path.exists(target):
-        return send_from_directory(static_folder, path)
-
-    return send_from_directory(static_folder, 'index.html')
